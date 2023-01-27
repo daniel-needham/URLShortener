244637ccff7710c7367174fe3c9a05b59bb35cef
dan <danielneedhamdn@gmail.com>
2023-01-27 16:33:57 +0000

    Implemented hash function

--- src/main/java/com/example/urlShortener/URLService.java
+++ src/main/java/com/example/urlShortener/URLService.java
@@ -3,16 +3,23 @@ package com.example.urlShortener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Optional;
+import jakarta.xml.bind.DatatypeConverter;
 
 @Service
 public class URLService {
     private final URLRepository urlRepository;
+    private final MessageDigest messageDigest;
+
+    private int urlSize = 4;
 
     @Autowired
-    public URLService(URLRepository urlRepository) {
+    public URLService(URLRepository urlRepository) throws NoSuchAlgorithmException {
         this.urlRepository = urlRepository;
+        this.messageDigest = MessageDigest.getInstance("MD5");
     }
 
     public List<URL> getAllURLs() {
@@ -34,21 +41,34 @@ public class URLService {
 
     //todo first check if the item is already there - (if not then hash) - and return newURL
     public URL tinyfySaveAndGetURL(String originalURL) {
-        System.out.println(originalURL);
-        String hash = hash(originalURL);
-        Optional<URL> optionalURL = urlRepository.findURLByTinyURL(hash);
-        URL newURL;
-        if (optionalURL.isPresent()) {
-            newURL = optionalURL.get();
-        } else {
-            newURL = new URL(originalURL, hash);
-            urlRepository.save(newURL);
+        URL newURL = null;
+
+        String hash = hash(originalURL, urlSize);
+        String tinyURL = hash.substring(0,urlSize);
+        Optional<URL> optionalURL = urlRepository.findURLByTinyURL(tinyURL);
+        int currentUrlSize = urlSize;
+        while (newURL == null && currentUrlSize < 32) {
+            if (optionalURL.isEmpty()) {
+                newURL = new URL(originalURL, tinyURL);
+                urlRepository.save(newURL);
+                break;
+            }
+            if (optionalURL.get().getOriginalURL().equals(originalURL)) {
+                newURL = optionalURL.get();
+                break;
+            }
+            tinyURL = hash.substring(0, ++currentUrlSize);
+            optionalURL = urlRepository.findURLByTinyURL(tinyURL);
+
         }
         return newURL;
     }
 
-    public String hash(String originalURL) {
-        //todo implement hash
-        return "11111";
+    public String hash(String originalURL, int length) {
+        byte[] urlBytes = originalURL.getBytes();
+        messageDigest.update(urlBytes);
+        byte[] digest = messageDigest.digest();
+        String fullHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+        return fullHash;
     }
 }
